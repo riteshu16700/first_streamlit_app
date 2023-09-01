beb43e3ad2a8be7a38f9f01afbf74d8ae121582f
riteshu16700 <142109958+riteshu16700@users.noreply.github.com>
2023-09-01 23:33:31 +0100

    Update streamlit_app.py
    added new menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit 
 
 streamlit.title('My Parents new healthy diner report') 
+
+streamlit.header('Breakfast Menu') 
+
+streamlit.text('Omega 3 and Blueberry Oatmeal') 
+streamlit.text('Kale, Spinach & Rocket Smoothie') 
+streamlit.text('Hard boiled and Free range eggs') 
+
